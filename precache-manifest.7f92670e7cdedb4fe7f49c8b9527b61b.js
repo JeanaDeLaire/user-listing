1e5f32c4c85557d5e26ba998c559d33c22ddb32b
JeanaDeLaire <jeanadelaire@gmail.com>
2020-01-12 18:52:21 -0500

    Updates

---
+++ precache-manifest.7f92670e7cdedb4fe7f49c8b9527b61b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e107c08b594e0e420c599bca49ed329",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1411671160b1cdf8a91b",
+    "url": "/static/css/main.fac0adce.chunk.css"
+  },
+  {
+    "revision": "e0694bfd57373cbe142d",
+    "url": "/static/js/2.6a39188e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6a39188e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1411671160b1cdf8a91b",
+    "url": "/static/js/main.e65dec22.chunk.js"
+  },
+  {
+    "revision": "f96efaf2326f7fc51046",
+    "url": "/static/js/runtime-main.e2641a1c.js"
+  }
+]);
